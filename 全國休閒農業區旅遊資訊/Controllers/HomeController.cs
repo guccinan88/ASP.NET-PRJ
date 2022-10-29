92678c9c86294a781f9c606d2982075dca0ffcc6
guccinan <guccinan@yahoo.com.tw>
2022-10-30 05:18:39 +0800

    first

---
+++ 全國休閒農業區旅遊資訊/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebApplication12.Models;
+
+namespace WebApplication12.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string path = $"{Server.MapPath("JSON").Replace("Home\\", "")}\\ODwsvAttractions.json";
+            StreamReader streamReader=new StreamReader(path);
+            FarmAttractions[] farmAttractions = JsonConvert.DeserializeObject<FarmAttractions[]>(streamReader.ReadToEnd());
+            HashSet<string> list=new HashSet<string>();
+            
+            foreach(var i in farmAttractions)
+            {
+                list.Add(i.City);
+            }
+            ViewBag.City = list;
+            return View(farmAttractions.OrderBy(m => m.City).ThenBy(m => m.Town).ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(string selectCity)
+        {
+            string path = $"{Server.MapPath("JSON").Replace("Home\\", "")}\\ODwsvAttractions.json";
+            StreamReader streamReader = new StreamReader(path);
+            FarmAttractions[] farmAttractions = JsonConvert.DeserializeObject<FarmAttractions[]>(streamReader.ReadToEnd());
+            HashSet<string> list = new HashSet<string>();
+
+            foreach (var i in farmAttractions)
+            {
+                list.Add(i.City);
+            }
+            ViewBag.City = list;
+            return View(farmAttractions.OrderBy(m => m.City).ThenBy(m => m.Town).Where(m=>m.City==selectCity).ToList());
+        }
+        
+    }
+}
---
+++ 全國休閒農業區旅遊資訊/Models/FarmAttractions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication12.Models
+{
+    public class FarmAttractions
+    {
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string TrafficGuidelines { get; set; }
+        public string Address { get; set; }
+        public string OpenHours { get; set; }
+        public string City { get; set; }
+        public string Town { get; set; }
+        public string Coordinate { get; set; }
+        public string Photo { get; set; }
+    }
+}
